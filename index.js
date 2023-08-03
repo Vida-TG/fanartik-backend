eaa4d6c61c3e671e4ec2e78b366f3f2ec45b2fcd
Vida-TG <victorademiju.t@gmail.com>
2023-08-04 00:49:16 +0100

    Path

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ mongoose
 
 const app = express();
 
-
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
