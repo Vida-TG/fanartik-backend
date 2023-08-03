28a5284aaa2eb82dc282eef0c33d1fb60cec51f6
Vida-TG <victorademiju.t@gmail.com>
2023-08-03 18:55:11 +0100

    OTR

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -6,8 +6,8 @@ import path from 'path';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
